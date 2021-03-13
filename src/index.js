3c6d665fee86d55a92bf6cd7de87acf71339d2a0
NewBavli <vlad.babich.97@inbox.ru>
2021-03-13 21:40:05 +0300

    init: add code;

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    var min_value = null;
+    if (!array || !array.length) return 0;
+    for (let i=0; i<array.length; i++){
+        if (min_value > array[i]) min_value=array[i];
+    }
+  return min_value;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) {   
+    var max_value = null;
+    if (!array || !array.length) return 0;
+    for (let i=0; i<array.length; i++){
+        if (max_value < array[i]) max_value=array[i];
+    }
+    return max_value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) return 0; 
+    var avg_value = 0;
+    if (array.length == 0) avg_value=0;
+    for (var i=0; i<array.length; i++){
+        avg_value= avg_value+array[i];
+    }
+    avg_value = avg_value/i;
+    return avg_value;
 }
